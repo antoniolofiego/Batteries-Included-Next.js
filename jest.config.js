7c6059119449d3dacf854b4f2363900dc5e4c52a
Antonio Lo Fiego <alofiego@hey.com>
2022-03-11 10:10:41 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,32 @@
 module.exports = {
-	testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-	setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-	transform: {
-		'^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-	},
-	moduleNameMapper: {
-		'\\.(css|less|scss|sass)$': '<rootDir>/styles/__mocks__/styleMock.js',
-		'^@pages/(.*)$': '<rootDir>/pages/$1',
-		'^@components/(.*)$': '<rootDir>/components/$1',
-	},
-	testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules)
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+    // Handle image imports
+    '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': `<rootDir>/__mocks__/fileMock.js`,
+
+    // Handle module aliases
+    '^@pages/(.*)$': '<rootDir>/pages/$1',
+    '^@components/(.*)$': '<rootDir>/components/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
 };
